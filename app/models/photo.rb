cd9e4114fc2794e6e46b0d2ecd40c3af40332607
mairinjerse <mairinjerse@gmail.com>
2023-03-30 20:23:19 +0000

    added validations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,9 @@ class Photo < ApplicationRecord
   belongs_to :owner, class_name: "User", counter_cache:true 
   has_many :comments
   has_many :likes 
-  has_many :fans, through: :likes, 
+  has_many :fans, through: :likes 
+
+  validates :captions, presence: true 
+  validates :image, presence: true 
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_29_165250) do
+ActiveRecord::Schema.define(version: 2023_03_30_202043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -63,11 +63,12 @@ ActiveRecord::Schema.define(version: 2023_03_29_165250) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.citext "username"
-    t.boolean "private"
+    t.boolean "private", default: true
     t.integer "likes_count"
     t.integer "comments_count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "photos_count"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true

9159bdd4544d6c29da3dc9db376470f86e70a28c
mairinjerse <mairinjerse@gmail.com>
2023-03-30 20:01:00 +0000

    added has_many

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ApplicationRecord
   belongs_to :owner, class_name: "User", counter_cache:true 
-  #has_many :comments
-  #has_many :fans, through: :likes 
+  has_many :comments
+  has_many :likes 
 end

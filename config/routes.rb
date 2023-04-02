7b93694ba9e0b73a97b1a6341ac1df8cd8ee5814
mairinjerse <mairinjerse@gmail.com>
2023-04-02 16:23:14 +0000

    starting to add links

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :comments
   resources :photos
   devise_for :users
-  get "/:username" => "users#show"
+
+  get ":username/liked" => "photos#liked", as: :liked_photos 
+  get "/:username" => "users#show", as: :user 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

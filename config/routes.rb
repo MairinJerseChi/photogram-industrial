203731beda05d350edd21fcb71ce812a8ce65d2f
mairinjerse <mairinjerse@gmail.com>
2023-04-02 16:07:12 +0000

    added right url

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   root "photos#index"
-
    #generate resources for only the user show page! 
-  resources :users, only: :show 
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
   devise_for :users
+  get "/:username" => "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
